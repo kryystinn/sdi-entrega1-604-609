c0519826e1575d0d8f10d4eaf6ac10ddd8713061
Cristina Ruiz de Bucesta <cristinaruizdebucesta@gmail.com>
2019-03-16 08:30:40 +0100

    Algunos tests implementados (funcionan) :)

--- MyWallapop/src/test/java/com/uniovi/tests/MyWallapopTests.java
+++ MyWallapop/src/test/java/com/uniovi/tests/MyWallapopTests.java
@@ -5,9 +5,15 @@ import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.FixMethodOrder;
+import org.junit.Test;
 import org.junit.runners.MethodSorters;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+
+import com.uniovi.tests.pageobjects.PO_HomeView;
+import com.uniovi.tests.pageobjects.PO_Properties;
+import com.uniovi.tests.pageobjects.PO_RegisterView;
+import com.uniovi.tests.pageobjects.PO_View;
 
 //Ordenamos las pruebas por el nombre del método
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
@@ -51,6 +57,47 @@ public class MyWallapopTests {
 	@AfterClass
 	static public void end() {
 		driver.quit();
+	}
+	
+	
+	
+	
+	//PR02. Acceder a la página principal /
+	@Test
+	public void PR01() {
+		PO_HomeView.checkWelcome(driver, PO_Properties.getSPANISH());
+	}
+	
+	//PR02. Opción de navegación. Pinchar en el enlace Registro en la página principal.
+	@Test
+	public void PR02() {
+		PO_HomeView.clickOption(driver, "signup", "class", "btn btn-primary");
+	}
+	
+	//PR03. Opción de navegación. Pinchar en el enlace Iniciar sesión en la página principal (que debería de ser el mismo).
+	@Test
+	public void PR03() {
+		PO_HomeView.clickOption(driver, "login", "class", "btn btn-primary");
+	}
+	
+	//PR04. Opción de navegación. Cambio de idioma de Español a Ingles y vuelta a Español
+	@Test
+	public void PR04() {
+		PO_HomeView.checkChangeIdiom(driver, "btnSpanish", "btnEnglish",
+				PO_Properties.getSPANISH(), PO_Properties.getENGLISH());
+		//SeleniumUtils.esperarSegundos(driver, 2);
+	}
+	
+	//PR05. Prueba del formulario de registro. Registro con datos correctos.
+	@Test
+	public void PR05() {
+		//Vamos al formulario de registro
+		PO_HomeView.clickOption(driver, "signup", "class", "btn btn-primary");
+		//Rellenamos el formulario.
+		PO_RegisterView.fillForm(driver, "prueba@gmail.com", "Soy una", "Prueba", "123456",
+				"123456");
+		//Comprobamos que entramos en home
+		PO_View.checkElement(driver, "text", "Ofertas");
 	}
 
 
