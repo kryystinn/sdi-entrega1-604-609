a3a0c9262e97faeb24f3e151e80c46274d6be293
Cristina Ruiz de Bucesta <cristinaruizdebucesta@gmail.com>
2019-03-16 06:42:18 +0100

    MENSAJE DE QUE NO PUEDES COMPRAR UN ARTÍCULO SÍIIIIIIIIIII

--- MyWallapop/src/main/java/com/uniovi/controllers/UsersController.java
+++ MyWallapop/src/main/java/com/uniovi/controllers/UsersController.java
@@ -165,8 +165,10 @@ public class UsersController {
 
 		}
 		
-		else
+		else {
 			session.setAttribute("error", "Error.buy.money");
+			return "/home :: tableOffers";
+		}
 //		if (offerToBuy.getUser() == null)
 //			session.setAttribute("error", "Error.buy.seller");
 		
